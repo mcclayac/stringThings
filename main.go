e8a1987a9ffad98a51ac842b59044786dc8f35ab
mcclayac <anthony.mcclay@me.com>
2018-12-23 07:30:12 -0600

    Added another for statement

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func main () {
+func main() {
 	atoz := "the quick brown fox jumps over the lazy dog\n"
 
 	fmt.Printf("%s\n", atoz[0:9])
-	fmt.Printf("%s\n", atoz[:9])  // Default to the beginning of the string
-	fmt.Printf("%s\n", atoz[15:19])  // Default to the beginning of the string
-	fmt.Printf("%s\n", atoz[15:])  // Take me to the end
+	fmt.Printf("%s\n", atoz[:9])    // Default to the beginning of the string
+	fmt.Printf("%s\n", atoz[15:19]) // Default to the beginning of the string
+	fmt.Printf("%s\n", atoz[15:])   // Take me to the end
 
 	//for i, r := range atoz {
 	//	fmt.Printf("index: %d \tCharacter: %c \n", i, r)
@@ -17,6 +17,11 @@ func main () {
 	for _, r := range atoz {
 		fmt.Printf("\tCharacter: %c \n", r)
 	}
+
+	for _, s := range atoz {
+		fmt.Printf("\t\t\tCharacter: %c \n", s)
+	}
+
 	fmt.Printf("length of ayoz: %d\n", len(atoz))
 
 	// BAckquote for double quotes
